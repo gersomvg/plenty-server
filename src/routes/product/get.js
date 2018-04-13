f70a47c32721e0ef756f48fbf962f029e7abd133
Gersom van Ginkel <gersomvg@gmail.com>
2018-04-13 11:46:24 +0200

    Added pagination to products

--- src/routes/product/get.js
+++ src/routes/product/get.js
@@ -1,3 +1,5 @@
+const Joi = require('joi');
+
 const Product = require('../../models/product');
 const unaccent = require('../../utils/unaccent');
 const escapeWhereLikeInput = require('../../utils/escapeWhereLikeInput');
@@ -5,12 +7,36 @@ const escapeWhereLikeInput = require('../../utils/escapeWhereLikeInput');
 const processUserInput = string => escapeWhereLikeInput(unaccent(string));
 const getLikeString = string => (string ? `%${processUserInput(string)}%` : '%%');
 
+const querySchema = Joi.object().keys({
+    name: Joi.string(),
+    limit: Joi.number()
+        .integer()
+        .min(1)
+        .max(100),
+    offset: Joi.number().integer(),
+});
+
 module.exports = async (req, res) => {
     try {
+        const validation = Joi.validate(req.query, querySchema);
+        if (validation.error) {
+            return res.status(400).send({error: 'Invalid query parameters provided'});
+        }
+
+        const limit = Number(req.query.limit || 25);
+        const offset = Number(req.query.offset || 0);
+
         const products = await Product.query()
             .where('name_unaccented', 'ilike', getLikeString(req.query.name))
+            .range(offset, limit + offset)
             .eager('brand');
-        res.send({items: products});
+
+        const hasNext = products.total > limit + offset;
+        const nextLink = hasNext
+            ? `${process.env.BASE_URL}/v2/product?limit=${limit}&offset=${limit + offset}`
+            : null;
+
+        res.send({items: products.results, nextLink});
     } catch (e) {
         console.error('❌  GET /product: ', e.message);
         res.status(500).send({error: 'Something went wrong'});
